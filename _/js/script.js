69330a7b596fcfd7c8421e2a7516bd4e9ccb5e8d
Ishtiak Sami <ishtiaksami.dev@gmail.com>
2024-09-18 20:09:51 +0600

    first commit

---
+++ _/js/script.js
@@ -0,0 +1,24 @@
+// swiper Silder
+
+new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  autoplay: {
+    delay: 4000, // Delay in milliseconds between slides
+    disableOnInteraction: false, // Autoplay will be disabled after user interactions
+  },
+  speed: 1000, // Transition speed in milliseconds
+  loop: true, // Enable continuous loop mode
+  effect: "fade", // Change slide transition effect to fade
+  fadeEffect: {
+    crossFade: true, // Enable smooth fade transition
+  },
+});
+
+// swiper Silder
