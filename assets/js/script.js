f5f2ef3b97e99c3075b787902226482aefd3aaf8
ishtiaksami <ishtiaksami.dev@gmail.com>
2025-02-19 20:22:39 +0600

    some pages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,72 @@
-// swiper Silder
-
-new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    hide: true,
-  },
-  autoplay: {
-    delay: 4000, // Delay in milliseconds between slides
-    disableOnInteraction: false, // Autoplay will be disabled after user interactions
-  },
-  speed: 1000, // Transition speed in milliseconds
-  loop: true, // Enable continuous loop mode
-  effect: "fade", // Change slide transition effect to fade
-  fadeEffect: {
-    crossFade: true, // Enable smooth fade transition
-  },
+$(window).on("scroll", function () {
+  const header = $("header");
+  if ($(this).scrollTop() > 0) {
+    header.addClass("scrolled");
+  } else {
+    header.removeClass("scrolled");
+  }
 });
 
-// swiper Silder
+$(document).ready(function () {
+  var header = $(".floating-header");
+  var sticky = header.offset().top;
+  var $menu = $(".mobile-menu");
+  var $menuButton = $(".mobile-menu-button");
+  var $lines = $menuButton.find("span");
+  var $header = $("header");
+
+  // Sticky Header on Scroll
+  $(window).scroll(function () {
+    header.toggleClass("fixed", window.pageYOffset > sticky);
+  });
+
+  // Toggle Menu Functionality
+  function openMenu() {
+    $menu.removeClass("hidden").css("maxHeight", "100vh");
+    $lines.eq(0).addClass("rotate-45 translate-y-1.5");
+    $lines.eq(1).addClass("opacity-0");
+    $lines.eq(2).addClass("-rotate-45 -translate-y-1.5");
+    $lines.css("margin", "-2px 0");
+    $header.addClass("menu-open-bg");
+  }
+
+  function closeMenu() {
+    $menu.css("maxHeight", "0px");
+    setTimeout(() => $menu.addClass("hidden"), 300);
+    $lines.removeClass(
+      "rotate-45 translate-y-1.5 opacity-0 -rotate-45 -translate-y-1.5"
+    );
+    $lines.css("margin", "5px 0");
+    $header.removeClass("menu-open-bg");
+  }
+
+  function resetMenuState() {
+    if (window.innerWidth > 767) {
+      closeMenu();
+      $("#menuIcon, #searchIcon, #closeIcon").removeClass("menu-active");
+    }
+  }
+
+  $menuButton.on("click", function () {
+    $menu.hasClass("hidden") ? openMenu() : closeMenu();
+    $("#menuIcon, #searchIcon, #closeIcon").toggleClass("menu-active");
+  });
+
+  $(window).on("resize", resetMenuState);
+});
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
+document.addEventListener("click", (e) => {
+  cursor.classList.add("expand");
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,38 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      animation: {
+        moveCursor1: "moveCursor1 0.5s infinite alternate",
+        moveCursor2: "moveCursor2 0.5s forwards",
+      },
+      keyframes: {
+        moveCursor1: {
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.2)" },
+        },
+        moveCursor2: {
+          "100%": { transform: "scale(2)" },
+        },
+      },
+      backgroundImage: {
+        custom1:
+          "linear-gradient(104deg, rgba(34, 34, 34, 0.00) 55.8%, rgba(34, 34, 34, 0.50) 95.23%)",
+      },
       fontFamily: {
-        Satoshi: ["Roboto", "sans-serif"],
-        obviously: ["Roboto", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        lato: ["Lato", "serif"],
+        playwrite: ["Playwrite VN", "serif"],
+      },
+      fontSize: {
+        28: "1.75rem",
+        38: "2.375rem",
+        58: "3.625rem",
+      },
+      colors: {
+        maisonRouge: "#B0362F",
+        armstrongGold: "#B29559",
+        antiqueWhite: "#F6EFE7",
+        centuryGold: "#ECDBB1",
       },
       spacing: {
         30: "1.875rem",
